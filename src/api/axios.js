7ac438854a404787d0508dfb688287859de6b2e3
Samreen-verse <khurshidpeshawar37@gmail.com>
2026-03-04 14:26:49 +0500

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
+// create axios instance
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+// function to set the access token in Authorization header
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
+
 export default api;
